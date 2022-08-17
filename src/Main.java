07ae6b56dc5fa4b5ceb64bf8cbb728431774a4f1
CEMKAAS <zaroslikob@mail.ru>
2022-08-17 15:10:52 +0300

    End, add class main and readme

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SalesManager convert = new SalesManager(new int[]{10});
+
+        convert.max();
+        System.out.println(convert.max());
+    }
+}

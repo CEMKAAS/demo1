5fa9e1032161b6afc6da001479841e47135e9d5c
= <zaroslikob@mail.ru>
2022-09-03 22:55:49 +0300

    long

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SalesManager convert = new SalesManager(new int[]{10});
+        SalesManager convert = new SalesManager(new long[]{10});
 
         convert.max();
         System.out.println(convert.max());
